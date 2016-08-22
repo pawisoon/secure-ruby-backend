5c0ac889cb8d6004245a49cffdf55981c71d3407
pawisoon <pawi552@gmail.com>
2016-08-22 19:05:03 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # secure-ruby-backend
+Readme
+
+1. Install all dependences: 
+
+sudo apt-get upgrade; sudo apt-get install gem; sudo apt-get install rubygems; sudo gem install ruby-full; sudo gem install rubygems-update;sudo gem install sinatra; sudo gem install base64; sudo gem install json; sudo gem install mysql; sudo gem install digest;sudo gem install securerandom;
+
+
+2. Create database and tables: 
+
+mysql -u root -p
+
+CREATE DATABASE myservice;
+
+
+CREATE TABLE `users` (
+  `uid` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `encrypted_password` varchar(160) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `salt` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`uid`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
+3. Edit script with new creditentials and 
---
+++ api.rb
@@ -0,0 +1,128 @@
+#!/usr/bin/env ruby
+require 'rubygems'
+require 'base64'
+require 'sinatra'
+require 'json'
+require 'mysql'
+require 'digest'
+require 'securerandom'
+require 'socket'
+require 'uri'
+
+locale = 'localhost'
+user = 'root'
+passwd = '*********'
+db = 'myservice'
+
+get '/' do
+	"Nothing to see here. Move along."
+end
+
+def check_registered_user_email(email)
+	begin
+		con = Mysql.new(locale,user,passwd,db) 
+		con.autocommit false
+
+		pst = con.prepare "SELECT * FROM users WHERE email = ?"
+
+		pst.execute "#{email}"
+			
+		n_rows = pst.num_rows
+
+		con.commit
+		return n_rows
+	rescue Mysql::Error => e
+		puts e
+		con.rollback
+	
+	ensure
+		pst.close if pst
+		con.close if con
+	end
+end
+
+def hashSSHA(password)
+	salt = Digest::SHA512.hexdigest SecureRandom.hex
+	salt = salt[0,10]
+	a = password+salt
+	en = Digest::SHA512.hexdigest a
+	b = en+salt
+	hash = [salt,b]
+	puts b
+	return hash
+end
+
+def checkhashSSHA(salt, password)
+	a = password+salt
+	en = Digest::SHA512.hexdigest a
+	b = en+salt
+	return b
+end
+
+def register_new_user(name,email,password)
+	begin
+		con = Mysql.new(locale,user,passwd,db) 
+		pst = con.prepare "INSERT INTO users(name, email, encrypted_password, salt, created_at) VALUES(?,?,?,?,?)"
+		hash = hashSSHA(password)
+		encrypted_password = hash.at(1)
+		salt = hash.at(0)
+		time = Time.new
+		pst.execute name, email, encrypted_password, salt, time.strftime("%Y-%m-%d %H:%M:%S")
+		"{\"success\":1,\"error\":\"User registered\"}"
+	rescue Mysql::Error => e
+		puts e
+		con.rollback
+	ensure
+		con.close if con
+		pst.close if pst
+	end
+end
+#login curl -i -X POST -H 'Content-Type: application/json' -d '{"tag":"login","email":"pawi552@gmail.com","password":"123"}' http://localhost:4567/mobilelogin
+#register curl -i -X POST -H 'Content-Type: application/json' -d '{"tag":"register","name":"Pawel","email":"pawi552@gmail.com","password":"123"}' http://localhost:4567/mobilelogin
+post '/mobilelogin' do
+	values = JSON.parse(request.env["rack.input"].read)
+	if values["tag"]=="login"
+		begin
+			con = Mysql.new(locale,user,passwd,db) 
+			con.autocommit false
+			rs = con.query "SELECT * FROM users WHERE email = '"+values["email"]+"'"
+			if rs.num_rows == 1
+				rs.each_hash do |row|
+				salt = row['salt']
+				encrypted_password = row['encrypted_password']
+				password = values["password"]
+				hash = checkhashSSHA(salt, password)
+				puts encrypted_password, "\n"+hash, "\n"+salt
+				if encrypted_password == hash
+					message = "Welcome "+row['name']
+					return "{\"success\":1,\"message\":\"#{message}\"}"
+				else
+					return"{\"success\":0,\"message\":\"Wrong password\"}"
+				end
+			end
+			con.commit
+			else
+				return"{\"success\":0,\"message\":\"User doesn't exist.\"}"
+			end
+		rescue Mysql::Error => e
+			puts e
+			con.rollback
+		ensure
+			con.close if con
+		end
+	elsif values["tag"]=="register"
+		if check_registered_user_email(values["email"]) > 0
+			"{\"success\":0,\"message\":\"User already exists\"}"
+		else
+			if values["name"]!="" && values["email"]!="" && values["password"]!=""
+				register_new_user(values["name"],values["email"],values["password"])
+				return "{\"success\":1,\"message\":\"User registered.\"}"
+			else
+				return "{\"success\":0,\"message\":\"Please provide all your details.\"}"
+			end
+		end
+	else
+		"{\"success\":0,\"message\":\"acces denied\"}"
+	end
+end
+
